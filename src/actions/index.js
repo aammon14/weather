92bdb29e6c141221f35ad6a380428de90b0287c1
Andrew Ammon <andrewammon@Andrews-MBP.home>
2018-05-19 17:01:11 -0400

    serach bar working, set up api key and request

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'b736ef971c51a298b53cf02b96b43ea9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
